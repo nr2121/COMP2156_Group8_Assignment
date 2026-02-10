c4285d9d46e41c9177aa5ce8ac0f4629b7785464
Alexis Mai <144744432+AlexisMalori@users.noreply.github.com>
2026-02-10 13:02:50 -0500

    Added timeofday.py which grabs the current time of day and prints it.

---
+++ timeofday.py
@@ -0,0 +1,6 @@
+# Simple python script to print the current time of day.
+
+from datetime import datetime
+
+# Print current time as day-month-year hour-minute-second
+print(datetime.now().strftime("%d-%m-%Y %H-%M-%S"))
