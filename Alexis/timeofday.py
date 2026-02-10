d61abfbfbe9d161c701a844bd0ca513461d41f19
Alexis Mai <144744432+AlexisMalori@users.noreply.github.com>
2026-02-10 13:44:41 -0500

    Deleted Alexis folder and contents. Wasn't necessary for the assignment.

--- Alexis/timeofday.py
+++
@@ -1,7 +0,0 @@
-# Simple python script to print the current time of day.
-
-from datetime import datetime
-
-# Print current time as day-month-year hour-minute-second
-print("The current time of day is:")
-print(datetime.now().strftime("%d-%m-%Y %H-%M-%S"))
